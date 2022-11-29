21ff823f66845f561ea6ec7609ff89d5da0de5a7
aixin <1454622738@qq.com>
2022-11-29 09:40:05 +0800

    修改园区企业列表接口

--- test_cases/test_property_list.py
+++ test_cases/test_property_list.py
@@ -6,7 +6,7 @@ from common.parameterize_util import ddt, read_testcase
 
 
 class Test_Property_List:
-    # @pytest.mark.run(order=2)  # x是整数(可以是正数也可以是负数)
+    @pytest.mark.run(order=2)  # x是整数(可以是正数也可以是负数)
     # 执行失败后重试， 重试3次，每次重试间隔2秒
     # @pytest.mark.flaky(reruns=3, reruns_delay=2)
     @pytest.mark.parametrize("args_name", read_testcase('\\property_list_data.yaml'))  ##读取测试用例的product_list_data.yaml文件中的参数及值，赋值给变量args_name
--- test_cases/test_register.py
+++ test_cases/test_register.py
@@ -10,7 +10,6 @@ from common.parameterize_util import ddt, read_testcase
 class Test_Register:
     # 执行失败后重试， 重试3次，每次重试间隔2秒
     @pytest.mark.flaky(reruns=3, reruns_delay=2)
-    # @pytest.mark.skip('暂时不执行')
     @pytest.mark.parametrize("args_name", read_testcase('\\register_data.yaml'))  ##读取测试用例的get_token.yaml文件中的参数及值，赋值给变量args_name
     def test_register(self, args_name):
         res = RequestUtil("base_test_url", Test()).standard_yaml(args_name)
